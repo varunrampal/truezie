64a214eb278cdfaf7fe623b8370b7e491ed7505c
Varun Rampal <varun.rampall@gmail.com>
2020-12-07 10:47:51 -0800

    Updates

---
+++ precache-manifest.2988fadc840b8c2530d58d6cab9a6e1d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc55baf6ce061bd5e7af2a56d769e573",
+    "url": "/truezie/index.html"
+  },
+  {
+    "revision": "2f2c6d2a9cc1bb28ae39",
+    "url": "/truezie/static/css/6.e09375fe.chunk.css"
+  },
+  {
+    "revision": "06186f82fc187a76439e",
+    "url": "/truezie/static/css/main.5c9436e7.chunk.css"
+  },
+  {
+    "revision": "daa254fe495e8775dabd",
+    "url": "/truezie/static/js/0.12166350.chunk.js"
+  },
+  {
+    "revision": "5b42b9698f399f2b2ebf",
+    "url": "/truezie/static/js/10.b4690fc0.chunk.js"
+  },
+  {
+    "revision": "b2d353a3f91017eaacf1",
+    "url": "/truezie/static/js/11.881f5ef3.chunk.js"
+  },
+  {
+    "revision": "285ce5b8594bd5384edf",
+    "url": "/truezie/static/js/3.4d898f64.chunk.js"
+  },
+  {
+    "revision": "00a55d4a3727dba494a3",
+    "url": "/truezie/static/js/4.83b46782.chunk.js"
+  },
+  {
+    "revision": "fa1b2f80b3a935399782",
+    "url": "/truezie/static/js/5.2c6875b5.chunk.js"
+  },
+  {
+    "revision": "2f2c6d2a9cc1bb28ae39",
+    "url": "/truezie/static/js/6.054c898a.chunk.js"
+  },
+  {
+    "revision": "b4462ca495c76addfc2b",
+    "url": "/truezie/static/js/7.bea72a1b.chunk.js"
+  },
+  {
+    "revision": "cdba5046bf149a9cb70b",
+    "url": "/truezie/static/js/8.7ca5e9c7.chunk.js"
+  },
+  {
+    "revision": "76dbeec4b1c6db58d7bc",
+    "url": "/truezie/static/js/9.e0ea9107.chunk.js"
+  },
+  {
+    "revision": "06186f82fc187a76439e",
+    "url": "/truezie/static/js/main.bb79b642.chunk.js"
+  },
+  {
+    "revision": "8b616823da0b9f705338",
+    "url": "/truezie/static/js/runtime~main.45a81a7e.js"
+  },
+  {
+    "revision": "5363cedd15f817972104249f05e3dc3a",
+    "url": "/truezie/static/media/login-bg.5363cedd.png"
+  },
+  {
+    "revision": "d2a1ba6b8b3368027da39924c161724c",
+    "url": "/truezie/static/media/receipt.d2a1ba6b.png"
+  },
+  {
+    "revision": "d3303ad0bb928a74b60538266d921130",
+    "url": "/truezie/static/media/user.d3303ad0.png"
+  }
+]);
